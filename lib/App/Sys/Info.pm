a4b08a39fc1a6790b33d25d2bba9bc6e2a6aa99f
Burak Gürsoy <burak@cpan.org>
2010-04-11 05:23:59 +0300

    Added more information sections & tidied internals

--- lib/App/Sys/Info.pm
+++ lib/App/Sys/Info.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use vars qw( $VERSION );
 
-$VERSION = '0.11';
+$VERSION = '0.12';
 
 use constant CP_UTF8      => 65_001;
 use constant KB           =>   1024;
@@ -84,74 +84,140 @@ sub _probe {
     my $NA   = $self->NA;
     my $i    = $self->info;
     my $os   = $self->os;
+    my $pt   = $os->product_type;
+    my $proc = $self->_processors;
+    my $tz   = $os->tz;
     my @rv;
+
     push @rv,
-    [ 'Sys::Info Version'         => Sys::Info->VERSION                       ],
-    [ 'Perl Version'              => $i->perl_long                            ],
-    [ 'Host Name'                 => $os->host_name                           ],
-    [ 'OS Name'                   => $self->_os_name()                        ],
-    [ 'OS Version'                => $self->_os_version()                     ],
-    [ 'OS Manufacturer'           => $meta->{manufacturer}          || $NA    ],
-    [ 'OS Configuration'          => $os->product_type              || $NA    ],
-    [ 'OS Build Type'             => $meta->{build_type}            || $NA    ],
-    [ 'Running on'                => $self->_bitness()                        ],
+    [ 'Sys::Info Version' => Sys::Info->VERSION   ],
+    [ 'Perl Version'      => $i->perl_long        ],
+    [ 'Host Name'         => $os->host_name       ],
+    [ 'OS Name'           => $self->_os_name()    ],
+    [ 'OS Version'        => $self->_os_version() ],
     ;
+
+    push @rv, [ 'OS Manufacturer'  => $meta->{manufacturer} ] if $meta->{manufacturer};
+    push @rv, [ 'OS Configuration' => $pt                   ] if $pt;
+    push @rv, [ 'OS Build Type'    => $meta->{build_type}   ] if $meta->{build_type};
+
+    $self->_bitness(      \@rv );
+    $self->_current_user( \@rv );
+
+    if ( $os->is_windows ) {
+        push @rv, [ 'Registered Owner'        => $meta->{owner}        ] if $meta->{owner};
+        push @rv, [ 'Registered Organization' => $meta->{organization} ] if $meta->{organization};
+    }
+
+    push @rv, [ 'Product ID'     => $meta->{product_id}      ] if $meta->{product_id};
+    $self->_install_date( \@rv );
+    push @rv, [ 'System Up Time' => elapsed($os->tick_count) ] if $os->tick_count;
+
+    if ( $os->is_windows ) {
+        push @rv, [ 'System Manufacturer' => $meta->{system_manufacturer} ] if $meta->{system_manufacturer};
+        push @rv, [ 'System Model'        => $meta->{system_model}        ] if $meta->{system_model};
+    }
+
+    push @rv, [ 'System Type'  => $meta->{system_type} ] if $meta->{system_type};
+    push @rv, [ 'Processor(s)' => $proc                ] if $proc;
+
+    $self->_proc_meta(    \@rv );
+    $self->_bios_version( \@rv );
+
+    push @rv, [ 'Windows Directory' => $meta->{windows_dir} ] if $meta->{windows_dir};
+    push @rv, [ 'System Directory'  => $meta->{system_dir}  ] if $meta->{system_dir};
+    push @rv, [ 'Boot Device'       => $meta->{boot_device} ] if $meta->{boot_device};
+    push @rv, [ 'System Locale'     => $self->{LOCALE}      ] if $self->{LOCALE};
+    push @rv, [ 'Input Locale'      => $self->{LOCALE}      ] if $self->{LOCALE};
+    push @rv, [ 'Time Zone'         => $tz                  ] if $tz;
     push @rv,
-    [ 'Registered Owner'          => $meta->{owner}                 || $NA    ],
-    [ 'Registered Organization'   => $meta->{organization}          || $NA    ],
-    if $os->is_windows;
-    push @rv,
-    [ 'Product ID'                => $meta->{product_id}            || $NA    ],
-    [ 'Original Install Date'     => $self->_install_date()                   ],
-    [ 'System Up Time'            => elapsed($os->tick_count)       || $NA    ],
-    ;
-    push @rv,
-    [ 'System Manufacturer'       => $meta->{system_manufacturer}   || $NA    ],
-    [ 'System Model'              => $meta->{system_model}          || $NA    ],
-    if $os->is_windows;
-    push @rv,
-    [ 'System Type'               => $meta->{system_type}           || $NA    ],
-    [ 'Processor(s)'              => $self->_processors()           || $NA    ],
-    [ 'BIOS Version'              => $self->_bios_version()                   ],
-    ;
-    push @rv,
-    [ 'Windows Directory'         => $meta->{windows_dir}           || $NA    ],
-    if $os->is_windows;
-    ;
-    push @rv,
-    [ 'System Directory'          => $meta->{system_dir}            || $NA    ],
-    [ 'Boot Device'               => $meta->{boot_device}           || $NA    ],
-    [ 'System Locale'             => $self->{LOCALE}                || $NA    ],
-    [ 'Input Locale'              => $self->{LOCALE}                || $NA    ],
-    [ 'Time Zone'                 => $os->tz                        || $NA    ],
     [ 'Total Physical Memory'     => $self->_mb($meta->{physical_memory_total}    ) ],
     [ 'Available Physical Memory' => $self->_mb($meta->{physical_memory_available}) ],
     [ 'Virtual Memory: Max Size'  => $self->_mb($meta->{page_file_total}          ) ],
     [ 'Virtual Memory: Available' => $self->_mb($meta->{page_file_available}      ) ],
-    [ 'Virtual Memory: In Use'    => $self->_vm()                             ],
-    [ 'Page File Location(s)'     => $meta->{page_file_path}        || $NA    ],
-    [ 'Domain'                    => $os->domain_name               || $NA    ],
-    [ 'Logon Server'              => $os->logon_server              || $NA    ],
     ;
-    push @rv,
-    [ 'Windows CD Key'            => $os->cdkey                     || $NA    ],
-    [ 'Microsoft Office CD Key'   => $self->_office_cdkey()                   ],
-    if $os->is_windows;
+
+    $self->_vm( \@rv );
+
+    my $domain = $os->domain_name;
+    my $logon  = $os->logon_server;
+    my $ip     = $os->ip;
+
+    push @rv, [ 'Page File Location(s)' => $meta->{page_file_path} ] if $meta->{page_file_path};
+    push @rv, [ 'Domain'                => $domain                 ] if $domain;
+    push @rv, [ 'Logon Server'          => $logon                  ] if $logon;
+    push @rv, [ 'IP Address'            => $os->ip                 ] if $ip;
+
+    if ( $os->is_windows ) {
+        my $cdkey = $os->cdkey;
+        my $okey  = $self->_office_cdkey;
+        push @rv, [ 'Windows CD Key'          => $cdkey ] if $cdkey;
+        push @rv, [ 'Microsoft Office CD Key' => $okey  ] if $okey;
+    }
+
     return @rv;
+}
+
+sub _current_user {
+    my($self, $rv_ref) = @_;
+    my $os   = $self->os;
+    my $user = $os->login_name || return;
+    my $real = $os->login_name( real => 1 );
+    return if ! $user || ! $real;
+    my $display = $real && ($real ne $user) ? qq{$real ($user)} : $user;
+    $display .= $os->is_root ? q{ is an administrator} : q{};
+    push @{ $rv_ref }, [ 'Current User', $display ];
+    return;
+}
+
+sub _proc_meta {
+    my $self = shift;
+    my $data = shift;
+    my @cpu  = $self->cpu->identify;
+    my $prop = $cpu[0] || {};
+    my $load = $self->cpu->load;
+    my $L1   = $prop->{L1_cache}{max_cache_size};
+    my $L2   = $prop->{L2_cache}{max_cache_size};
+    my $sock = $prop->{socket_designation};
+    my $id   = $prop->{processor_id};
+    my @rv;
+
+    my $check_lc = sub {
+        my $ref = shift || return;
+        return if ! ${ $ref };
+        ${ $ref } .= q{ KB} if ${ $ref } !~ m{\sKB\z}xms;
+        return;
+    };
+
+    $check_lc->( \$L1 );
+    $check_lc->( \$L2 );
+
+    push @rv, qq{Load    : $load}  if $load;
+    push @rv, qq{L1 Cache: $L1}    if $L1;
+    push @rv, qq{L2 Cache: $L2}    if $L2;
+    push @rv, qq{Package : $sock}  if $sock;
+    push @rv, qq{ID      : $id}    if $id;
+
+    my $buf = q{ } x 2**2;
+    push @{$data}, [ q{ }, $buf . $_ ] for @rv;
+    return;
 }
 
 sub _processors {
     my $self = shift;
-    my $rv   = sprintf '%s ~%sMHz', scalar($self->cpu->identify), $self->cpu->speed;
+    my $cpu  = $self->cpu;
+    my $name = scalar $cpu->identify;
+    my $rv   = sprintf '%s ~%sMHz', $name, $cpu->speed;
     $rv =~ s{\s+}{ }xmsg;
     return $rv;
 }
 
 sub _vm {
-    my $self = shift;
-    my $tot  = $self->meta->{page_file_total}     || return $self->NA;
-    my $av   = $self->meta->{page_file_available} || return $self->NA;
-    return $self->_mb( $tot - $av );
+    my($self, $rv_ref) = @_;
+    my $tot = $self->meta->{page_file_total}     || return;
+    my $av  = $self->meta->{page_file_available} || return;
+    push @{ $rv_ref }, [ 'Virtual Memory: In Use' => $self->_mb( $tot - $av ) ];
+    return;
 }
 
 sub _mb {
@@ -177,26 +243,27 @@ sub _office_cdkey {
 }
 
 sub _bitness {
-    my $self = shift;
-    my %bit = (
-        cpu => $self->cpu->bitness || q{??},
-        os  => $self->os->bitness  || q{??},
-    );
-    return "$bit{cpu}bit CPU & $bit{os}bit OS";
+    my($self, $rv_ref) = @_;
+    my $cpu = $self->cpu->bitness || q{??};
+    my $os  = $self->os->bitness  || q{??};
+    push @{ $rv_ref }, [ 'Running on' => qq{${cpu}bit CPU & ${os}bit OS} ];
+    return;
 }
 
 sub _install_date {
-    my $self = shift;
-    return $self->meta->{install_date} ? scalar localtime $self->meta->{install_date} : $self->NA;
+    my($self, $rv_ref) = @_;
+    my $date = $self->meta->{install_date} || return;
+    push @{ $rv_ref }, [ 'Original Install Date' => scalar localtime $date ];
+    return;
 }
 
 sub _bios_version {
-    my $self = shift;
+    my($self, $rv_ref) = @_;
     local $@;
-    my $bv = eval {
-                $self->info->device('bios')->version;
-             };
-    return $bv || $self->NA;
+    my $bv = eval { $self->info->device('bios')->version; };
+    return if $@ || ! $bv;
+    push @{ $rv_ref }, [ 'BIOS Version' => $bv ];
+    return;
 }
 
 1;
